d490a042f90e36ffe841dddcc2b17cc23cde3889
Robin van Baalen <robin@ganttify.com>
2025-03-20 23:16:23 -0400

    feat: add @rvanbaalen/readme-to-html configuration

---
+++ rtoh.config.js
@@ -0,0 +1,8 @@
+/**
+ * Configuration file for readme-to-html
+ * Generated with the --init command
+ */
+export default {
+  templatePath: "https://raw.githubusercontent.com/rvanbaalen/rvanbaalen.github.io/refs/heads/main/templates/project-template.html",
+  stylesheetPath: "https://raw.githubusercontent.com/rvanbaalen/rvanbaalen.github.io/refs/heads/main/templates/style.css",
+}
